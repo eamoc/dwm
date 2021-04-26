00b8dc0195cbcb075209bca158af437ab3bda6e7
eamonn costello <eamonncostello@gmail.com>
2021-04-26 07:39:55 +0100

    Changed the font

--- config.def.h
+++ config.def.h
@@ -6,8 +6,8 @@ static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
-static const char *fonts[]          = { "InconsolataGo Nerd Font:style=Regular:size=12" };
-static const char dmenufont[]       = "InconsolataGo Nerd Font Mono:style=Regular:size=12";
+static const char *fonts[]          = { "JetBrainsMono Nerd Font:style=Regular:size=10" };
+static const char dmenufont[]       = "JetBrainsMono Nerd Font Mono:style=Regular:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
